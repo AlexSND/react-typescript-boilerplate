d6d1c8df467127ab68401017d4c46292a79208e0
Alexandr Serebrennikov <aserebrennikov@itsumma.ru>
2021-02-07 23:12:42 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/style.scss';
+
+const App: React.FC = () => (
+  <h1>
+    Happy coding!
+    <span role="img" aria-label="emoji">😎</span>
+  </h1>
+);
+
+export default App;
