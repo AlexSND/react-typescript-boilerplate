6fbe9d1c6f8b027c78e348afd75d949b9ae22a71
Alexandr Serebrennikov <aserebrennikov@itsumma.ru>
2021-02-08 22:10:52 +0800

    add react-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import './styles/style.scss';
 
 const App: React.FC = () => (
-  <h1>
-    Happy coding!
-    <span role="img" aria-label="emoji">😎</span>
-  </h1>
+  <>
+    <Helmet>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="app description" />
+      <title>React boilerplate</title>
+    </Helmet>
+    <h1>
+      Happy coding!
+      <span role="img" aria-label="emoji">😎</span>
+    </h1>
+  </>
 );
 
 export default App;
